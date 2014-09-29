2d36cb64154d72c28c6761f7653943fd650d58ff
Chris Warburton <chriswarbo@gmail.com>
2014-09-29 08:35:47 +0100

    Set dir

--- PanPipe.hs
+++ PanPipe.hs
@@ -2,6 +2,8 @@ module PanPipe where
 
 import Control.Applicative
 import Data.List
+import System.Directory
+import System.IO.Temp
 import System.Process
 import Text.Pandoc
 import Text.Pandoc.Walk (walkM)
@@ -24,7 +26,11 @@ noPipe (x, y, zs) = (x, y, filter ((/= key) . fst) zs)
 getPipe (_, _, as) = snd <$> find ((== key) . fst) as
 
 transform :: Pandoc -> IO Pandoc
-transform = walkM pipe
+transform = let f d = do setCurrentDirectory d
+                         transformDoc
+             in withSystemTempDirectory "panpipe" f
+
+transformDoc = walkM pipe
 
 readDoc :: String -> Pandoc
 readDoc = readMarkdown def
