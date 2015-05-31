5ab44b49511d0d8cb9f05c0615bb8ba60841db35
Chris Warburton <chriswarbo@gmail.com>
2015-05-31 12:57:17 +0100

    Use cabal

--- default.nix
+++ default.nix
@@ -1,22 +1,20 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "panpipe";
-
+{ mkDerivation, base, binary, bytestring, Cabal, containers
+, data-default, directory, extensible-exceptions, filepath, mtl
+, network, old-locale, old-time, pandoc, pandoc-types, parsec
+, process, random, stdenv, temporary, text, time, unix
+}:
+mkDerivation {
+  pname = "panpipe";
+  version = "0.1.0.0";
   src = ./.;
-
-  buildInputs = [
-    haskellPackages.ghc
-    haskellPackages.pandoc
-    haskellPackages.QuickCheck
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [
+    base binary bytestring Cabal containers data-default directory
+    extensible-exceptions filepath mtl network old-locale old-time
+    pandoc pandoc-types parsec process random temporary text time unix
   ];
-
-  buildPhase = ''
-    ghc --make panpipe.hs
-  '';
-
-  installPhase = ''
-    mkdir -p "$out/bin"
-    cp panpipe "$out/bin/"
-  '';
+  homepage = "http://chriswarbo.net/essays/activecode";
+  description = "Pandoc filter to execute code blocks";
+  license = stdenv.lib.licenses.publicDomain;
 }
---
+++ shell.nix
@@ -0,0 +1,24 @@
+with (import <nixpkgs> {}).pkgs;
+let pkg = haskellngPackages.callPackage
+            ({ mkDerivation, base, binary, bytestring, Cabal, containers
+             , data-default, directory, extensible-exceptions, filepath, mtl
+             , network, old-locale, old-time, pandoc, pandoc-types, parsec
+             , process, random, stdenv, temporary, text, time, unix
+             }:
+             mkDerivation {
+               pname = "PanPipe";
+               version = "0.1.0.0";
+               src = ./.;
+               isLibrary = false;
+               isExecutable = true;
+               buildDepends = [
+                 base binary bytestring Cabal containers data-default directory
+                 extensible-exceptions filepath mtl network old-locale old-time
+                 pandoc pandoc-types parsec process random temporary text time unix
+               ];
+               homepage = "http://chriswarbo.net/essays/activecode";
+               description = "Pandoc filter to execute code blocks";
+               license = stdenv.lib.licenses.publicDomain;
+             }) {};
+in
+  pkg.env
