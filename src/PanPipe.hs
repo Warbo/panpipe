2cb0455e5e0503f55c23e54169171a227d074b95
Chris Warburton <chriswarbo@gmail.com>
2015-09-17 16:53:42 +0100

    Don't buffer stderr

---
+++ src/PanPipe.hs
@@ -0,0 +1,48 @@
+module PanPipe where
+
+import Control.Applicative
+import Data.List
+import System.Exit
+import System.IO
+import System.IO.Temp (withSystemTempDirectory)
+import System.Posix
+import System.Process
+import Text.Pandoc
+import Text.Pandoc.Shared (inDirectory)
+import Text.Pandoc.Walk (walkM)
+
+pipeBWith :: (Functor m, Monad m) => (String -> String -> m String)
+                                  -> Block
+                                  -> m Block
+pipeBWith f (CodeBlock as s)
+          |  Just (as', p) <- partPipes as = CodeBlock as' <$> f p s
+pipeBWith f x = walkM (pipeIWith f) x
+
+pipeB = pipeBWith readShell
+
+pipeIWith :: (Functor m, Monad m) => (String -> String -> m String)
+                                  -> Inline
+                                  -> m Inline
+pipeIWith f (Code as s)
+          |  Just (as', p) <- partPipes as = Code as' <$> f p s
+pipeIWith f x = return x
+
+pipeI = pipeIWith readShell
+
+readShell :: FilePath -> String -> IO String
+readShell p s = readProcess "sh" ["-c", p] s
+
+partPipes :: Attr -> Maybe (Attr, String)
+partPipes (x, y, zs) = case partition (("pipe" ==) . fst) zs of
+                            ((_, p):_, zs') -> Just ((x, y, zs'), p)
+                            _               -> Nothing
+
+transform :: Pandoc -> IO Pandoc
+transform doc = do cwd <-getWorkingDirectory
+                   withSystemTempDirectory
+                       "panpipe"
+                       (\dir -> do createSymbolicLink cwd (dir ++ "/root")
+                                   inDirectory dir (transformDoc doc))
+
+transformDoc :: Pandoc -> IO Pandoc
+transformDoc = walkM pipeB
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "pandoc" ]; then
-  git clone --depth=1 git://github.com/jgm/pandoc.git
-fi
-
-runhaskell -ipandoc/tests -- test-real.hs
