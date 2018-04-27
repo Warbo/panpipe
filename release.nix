1406a45b041e702d30b6e0e4c25943749f7a56c6
Chris Warburton <chriswarbo@gmail.com>
2018-04-27 18:51:11 +0100

    Use haskellRelease

--- release.nix
+++ release.nix
@@ -3,41 +3,32 @@
 # Provides a bunch of nixpkgs versions, augmented with useful helper functions
 with builtins;
 with rec {
-  pinnedConfig = (import <nixpkgs> { config = {}; }).fetchgit {
-    url    = http://chriswarbo.net/git/nix-config.git;
-    rev    = "7b96c3a";
-    sha256 = "0sqhf599ll2qshbnmspni5r3h5mfwbbkyz5xhfb3jxhflba594zw";
-  };
+  pkgSrc =
+    with tryEval <nix-config>;
+    if success
+       then value
+       else (import <nixpkgs> { config = {}; }).fetchgit {
+              url    = http://chriswarbo.net/git/nix-config.git;
+              rev    = "8d516f9";
+              sha256 = "1chsq3r7m5ppxk8hynv69sdimr9m1cpm2k1p29vwx3w9jdkajggq";
+            };
 
-  pkgSets = { pinnedConfig = import pinnedConfig {}; } //
-    (with tryEval <nix-config>;
-     if success
-        then { pathConfig = import value {}; }
-        else {});
-
+  pkgs = import pkgSrc {};
 };
 
-with pkgSets.pinnedConfig.nixpkgs1709.lib;
-with rec {
-  # "self" is a customised nixpkgs set, "super" is the corresponding original
-  buildForNixpkgs = self: super: mapAttrs (_: buildForHaskell self)
-                                          super.haskell.packages;
-
-  buildForHaskell = pkgs: hsPkgs: rec {
-    # Uses Haskell package set provided by nixpkgs
-    nixpkgsExpr  = pkgs.runCabal2nix { url = ./.; };   # Useful GC root
-    nixpkgsDeps  = hsPkgs.callPackage nixpkgsExpr {};  # Actual Haskell build
-
-    # Uses a Cabal sandbox to pick dependencies from (a snapshot of) Hackage
-    hackageDeps = pkgs.haskellPkgWithDeps {
-      inherit hsPkgs;
-      delay-failure = true;
-      dir           = ./.;
-    };
+with pkgs.lib;
+{
+  release = pkgs.haskellRelease {
+    name        = "panpipe";
+    dir         = ./.;
+    haskellKeep = hsVersion: !(hasPrefix "ghcjs"          hsVersion ||
+                               hasPrefix "lts"            hsVersion ||
+                               hasPrefix "ghc6"           hsVersion ||
+                               hasPrefix "ghc72"          hsVersion ||
+                               hasPrefix "ghcHaLVM"       hsVersion ||
+                               hasPrefix "integer-simple" hsVersion ||
+                               hasPrefix "ghcCross"       hsVersion ||
+                               hasSuffix "HEAD"           hsVersion);
+    nixKeep     = nixVersion: compareVersions nixVersion "nixpkgs1709" != -1;
   };
-
-  buildForPkgSet = pkgSet:
-    mapAttrs (name: self: buildForNixpkgs self (getAttr name pkgSet))
-             pkgSet.customised;
-};
-mapAttrs (_: buildForPkgSet) pkgSets
+}
