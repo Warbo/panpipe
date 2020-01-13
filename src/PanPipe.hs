7db15231949f7097b59f4e56aab609da0ee9b42e
Chris Warburton <chriswarbo@gmail.com>
2020-01-13 20:50:15 +0000

    Version 0.4.0.0
    Bump the pandoc and pandoc-types versions, and switch our uses of String to Text
    as appropriate.

--- src/PanPipe.hs
+++ src/PanPipe.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 module PanPipe where
 
 import Control.Applicative
 import Data.List
+import Data.Text (pack, Text, unpack)
 import System.Exit
 import System.IO
 import System.IO.Temp (withSystemTempDirectory)
@@ -12,28 +14,28 @@ import Text.Pandoc.JSON   (toJSONFilter)
 import Text.Pandoc.Shared (inDirectory)
 import Text.Pandoc.Walk   (walkM)
 
-pipeBWith :: (Functor m, Monad m) => (String -> String -> m String)
+pipeBWith :: (Functor m, Monad m) => (FilePath -> Text -> m Text)
                                   -> Block
                                   -> m Block
 pipeBWith f (CodeBlock as s)
-          |  Just (as', p) <- partPipes as = CodeBlock as' <$> f p s
+          |  Just (as', p) <- partPipes as = CodeBlock as' <$> f (unpack p) s
 pipeBWith f x = walkM (pipeIWith f) x
 
 pipeB = pipeBWith readShell
 
-pipeIWith :: (Functor m, Monad m) => (String -> String -> m String)
+pipeIWith :: (Functor m, Monad m) => (FilePath -> Text -> m Text)
                                   -> Inline
                                   -> m Inline
 pipeIWith f (Code as s)
-          |  Just (as', p) <- partPipes as = Code as' <$> f p s
+          |  Just (as', p) <- partPipes as = Code as' <$> f (unpack p) s
 pipeIWith f x = return x
 
 pipeI = pipeIWith readShell
 
-readShell :: FilePath -> String -> IO String
-readShell p = readProcess "sh" ["-c", p]
+readShell :: FilePath -> Text -> IO Text
+readShell path stdin = pack <$> readProcess "sh" ["-c", path] (unpack stdin)
 
-partPipes :: Attr -> Maybe (Attr, String)
+partPipes :: Attr -> Maybe (Attr, Text)
 partPipes (x, y, zs) = case partition (("pipe" ==) . fst) zs of
                             ((_, p):_, zs') -> Just ((x, y, zs'), p)
                             _               -> Nothing
--- test/Main.hs
+++ test/Main.hs
@@ -1,35 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
 import           Control.Applicative
 import           Data.Maybe
+import qualified Data.Text as T
 import           PanPipe
 import           Text.Pandoc
+import           Test.QuickCheck (Arbitrary, arbitrary, shrink)
 import           Test.Tasty (defaultMain, testGroup)
 import           Test.Tasty.QuickCheck (testProperty)
 
-pipeBPass p s a1 a2 = let at x  = (undefined, undefined, a1 ++ x ++ a2)
-                          f a b = [a ++ b]
-                          [CodeBlock _ lhs] = pipeBWith f (CodeBlock (at [("pipe", p)]) s)
-                      in  lhs == p ++ s
+u = undefined
+
+(+++) = T.append
+
+pipeBPass p s a1 a2 = let at x  = (u, u, a1 ++ x ++ a2)
+                          f a b = [T.pack a +++ b]
+                          [CodeBlock _ lhs] = pipeBWith f
+                                                (CodeBlock (at [("pipe", p)]) s)
+                      in  lhs == p +++ s
 
 pipeBAttr i c a1 a2 = let at x = (i, c, a1 ++ x ++ a2)
-                          [CodeBlock as _] = pipeBWith (\_ _ -> [undefined])
-                                                       (CodeBlock (at [("pipe", undefined)])
-                                                                  undefined)
+                          [CodeBlock as _] = pipeBWith (\_ _ -> [u])
+                                               (CodeBlock (at [("pipe", u)])
+                                                                  u)
                       in  at [] == as
 
-pipeIPass p s a1 a2 = let at x  = (undefined, undefined, a1 ++ x ++ a2)
-                          f a b = [a ++ b]
+pipeIPass p s a1 a2 = let at x  = (u, u, a1 ++ x ++ a2)
+                          f a b = [T.pack a +++ b]
                           [Code _ lhs] = pipeIWith f (Code (at [("pipe", p)]) s)
-                      in  lhs == p ++ s
+                      in  lhs == p +++ s
 
 pipeIAttr i c a1 a2 = let at x = (i, c, a1 ++ x ++ a2)
-                          [Code as _] = pipeIWith (\_ _ -> [undefined])
-                                                  (Code (at [("pipe", undefined)])
-                                                        undefined)
+                          [Code as _] = pipeIWith (\_ _ -> [u])
+                                                  (Code (at [("pipe", u)])
+                                                        u)
                       in at [] == as
 
-nonPipe as = isNothing $ partPipes (undefined, undefined, filter ((/= "pipe") . fst) as)
+nonPipe as = isNothing $ partPipes (u, u, filter ((/= "pipe") . fst) as)
 
-pipeClass p as = (snd <$> partPipes (undefined, undefined, ("pipe", p):as)) == Just p
+pipeClass p as = (snd <$> partPipes (u, u, ("pipe", p):as)) == Just p
 
 main = defaultMain $ testGroup "All tests"
        [
@@ -40,3 +48,7 @@ main = defaultMain $ testGroup "All tests"
        , testProperty "non-pipes ignored"                    nonPipe
        , testProperty "pipe class found"                     pipeClass
        ]
+
+instance Arbitrary T.Text where
+  arbitrary = T.pack <$> arbitrary
+  shrink x  = map T.pack (shrink (T.unpack x))
