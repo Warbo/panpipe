24734f658036dc9fa01daf897e99a6955a2d391f
Chris Warburton <chriswarbo@gmail.com>
2020-01-13 20:52:30 +0000

    Use iohk's haskell.nix infrastructure
    It uses cabal to solve the dependencies, which prevents us having to maintain a
    list of dependency versions ourselves.

--- release.nix
+++ release.nix
@@ -1,10 +1,3 @@
 # Used for building and testing on build servers like Hydra
-with import ./nixpkgs.nix;
-with lib;
-with {
-  nixpkgsVersion = fileContents (path + "/.version");
-  ghcVersion     = haskellPackages.ghc.version;
-};
-{
-  "nixpkgs${nixpkgsVersion}-ghc${ghcVersion}-panpipe" = haskellPackages.panpipe;
-}
+with (import ./.).panpipe.components;
+exes // tests // { inherit library; }
