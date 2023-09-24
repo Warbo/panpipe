9f26bf36c2a641ebcd3393670db68ab7e3f66351
Chris Warburton <chriswarbo@gmail.com>
2023-09-24 09:31:51 +0200

    Use default args in default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,9 @@
-{ haskellPackages, lib }: haskellPackages.haskellSrc2nix {
-  name = "panpipe";
-  src  = lib.cleanSource ./.;
-}
+{ haskellPackages ? nixpkgs.haskellPackages, nixpkgs ? import (fetchTarball {
+  name = "nixpkgs2305";
+  url = "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz";
+  sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+}) {
+  config = { };
+  overlays = [ ];
+} }:
+haskellPackages.callCabal2nix "panpipe" ./. { }
