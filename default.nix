19e37791ff36a37117b5715cfd5f1b11471bdfbf
Chris Warburton <chriswarbo@gmail.com>
2023-12-04 14:31:15 +0000

    Use nixpkgs 23.11

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 { haskellPackages ? nixpkgs.haskellPackages, nixpkgs ? import (fetchTarball {
-  name = "nixpkgs2305";
-  url = "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz";
-  sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+  name = "nixpkgs2311";
+  url = "https://github.com/nixos/nixpkgs/archive/"
+    + "057f9aecfb71c4437d2b27d3323df7f93c010b7e.tar.gz";
+  sha256 = "1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
 }) {
   config = { };
   overlays = [ ];
